2e06f2cacf34d6600285c53182956751779dbf6f
Ben McCarthy <43456399+bendiscover@users.noreply.github.com>
2020-04-24 14:05:26 +1000

    Update two-up.php

--- two-up.php
+++ two-up.php
@@ -3,6 +3,16 @@
 	<head>
 		<script data-ad-client="ca-pub-2374904224864526" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164475509-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-164475509-1');
+</script>
+
 		<style>
 
 
